3fb629d4d13329ee669b5ac1f0b2a08e09018bbd
ByungchangSo <sinwall@snu.ac.kr>
2022-10-18 15:08:01 +0900

    error fixed

--- DataAugmentation.py
+++ DataAugmentation.py
@@ -14,6 +14,7 @@ class Reverter():
         X_aug, y_aug = X.copy(), y.copy()
         X_aug.iloc[:, 3:16] = -X_aug.iloc[:, 3:16]
         X_aug.iloc[:, 5] = -X_aug.iloc[:, 5]
+        X_aug['sequence'] += X['sequence'].max()+1
         X = pd.concat([X, X_aug], axis=0, ignore_index=True)
         y = np.concatenate([y, y_aug], axis=0)
         return X, y
@@ -30,6 +31,7 @@ class MultPerturb():
         rng = np.random.default_rng(self.random_state)
         X_aug, y_aug = X.copy(), y.copy()
         X_aug.iloc[:, 3:16] *= (0.9 + 0.2*rng.random(X_aug.iloc[:, 3:16].shape))
+        X_aug['sequence'] += X['sequence'].max()+1
         X = pd.concat([X, X_aug], axis=0, ignore_index=True)
         y = np.concatenate([y, y_aug], axis=0)
         return X, y
